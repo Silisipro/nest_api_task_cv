b90b7e9b26faedb29e75db869b0702b120b91bc2
HONFO Sylvestre <sylvestrehonfo4@gmail.com>
2024-10-20 23:42:18 +0100

    initialisation

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as morgan from 'morgan';
+import * as dotenv from 'dotenv';
+
+import { DurationInterceptor } from './interceptors/duration.interceptor';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
+dotenv.config();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const corsOptions = {
+    origin: [
+      'http://localhost:4200',
+    ]
+  }
+  app.enableCors(corsOptions);
+  app.use(morgan('dev'));
+  app.use(
+    (req: Request, res: Response, next) => {
+      console.log('Middelware from app.use');
+      next();
+    }
+  )
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transformOptions: {
+      enableImplicitConversion: true
+    }
+  }));
+  app.useGlobalInterceptors(new DurationInterceptor());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(new Reflector()));
+  // console.log('port : ', configService.get('port'));
+  const options = new DocumentBuilder()
+    .setTitle('Sylvestre cv')
+    .setDescription('Cv sylvestre application nestjs')
+    .setVersion('1.0')
+    .addTag('cv')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  //configService.get('port') ||
+  await app.listen( process.env.PORT || 3000);
+}
+bootstrap();
